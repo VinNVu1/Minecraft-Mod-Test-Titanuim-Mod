c66c7a8599ce091288f5504f9b7f8b9620cac8c5
VinNVu1 <vincentevu0989@icloud.com>
2023-08-20 21:02:18 -0700

    Added createMultiOreDrop method (WIP)

--- src/main/java/com/veeveemods/testmod/datagen/ModBlockLootTables.java
+++ src/main/java/com/veeveemods/testmod/datagen/ModBlockLootTables.java
@@ -2,9 +2,24 @@ package com.veeveemods.testmod.datagen;
 
 import com.veeveemods.testmod.block.ModBlocks;
 import com.veeveemods.testmod.item.ModItems;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.BeetrootBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
@@ -22,13 +37,61 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         add(ModBlocks.RUTILE_ORE_BLOCK.get(),
                 (block) -> createOreDrop(ModBlocks.RUTILE_ORE_BLOCK.get(), ModItems.RUTILE_ORE.get()));
 
-
-//        add(ModBlocks.DEEPSLATE_RUTILE_ORE_BLOCK.get(),
-//                (block) -> createOreDrop(ModBlocks.DEEPSLATE_RUTILE_ORE_BLOCK.get(), List.of(ModItems.RUTILE_ORE.get(), ModItems.RAW_IRON_FRAGMENT.get())));
-
         add(ModBlocks.DEEPSLATE_RUTILE_ORE_BLOCK.get(),
-                (block) -> createOreDrop(ModBlocks.DEEPSLATE_RUTILE_ORE_BLOCK.get(), ModItems.RUTILE_ORE.get()));
+                (block) -> createMultiOreDrop(ModBlocks.DEEPSLATE_RUTILE_ORE_BLOCK.get(), ModItems.RUTILE_ORE.get(), ModItems.RAW_IRON_FRAGMENTS.get()));
+
     }
+
+    /*
+    * TODO: Figure out this block of code.
+    *  We know we need 4 of those parameters but how can we utilize the builder?
+    *  I have a builder method we can use on line 66 but it is not modified yet.
+    *  Line 66 - 93 I'm using as references. It should give us something if we figure out how to combine them together.
+    * */
+    protected LootTable.Builder createMultiOreDrop(Block oreBlock, Item ore1, Item ore2, LootItemCondition.Builder builder) {
+        return createSilkTouchDispatchTable(oreBlock,
+                this.applyExplosionDecay(oreBlock,
+                        LootTable.lootTable()
+                                .withPool(LootPool.lootPool()
+                                        .add(LootItem
+                                                .lootTableItem(ore1).apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))))
+                                .withPool(LootPool.lootPool()
+                                        .add(LootItem
+                                                .lootTableItem(ore2).apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F)))
+                                                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE)))))
+        );
+
+    }
+
+    LootItemCondition.Builder lootitemcondition$builder = LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RUTILE_ORE_BLOCK.get()).setProperties(StatePropertiesPredicate.Builder.properties());
+    protected LootTable.Builder createOreDrop(Block oreBlock, Item ore1) {
+        return createSilkTouchDispatchTable(oreBlock, this.applyExplosionDecay(oreBlock,
+                LootItem.lootTableItem(ore1)
+                        .apply(ApplyBonusCount
+                                .addOreBonusCount(Enchantments.BLOCK_FORTUNE))));
+    }
+    protected LootTable.Builder createCropDrops(Block cropBlock, Item wheat, Item seed, LootItemCondition.Builder builder) {
+        return this.applyExplosionDecay(cropBlock,
+                LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .add(LootItem
+                                .lootTableItem(wheat)
+                                .when(builder)
+                                .otherwise(LootItem.lootTableItem(seed))))
+                .withPool(LootPool
+                        .lootPool()
+                        .when(builder)
+                        .add(LootItem.lootTableItem(seed)
+                                .apply(ApplyBonusCount
+                                        .addBonusBinomialDistributionCount(Enchantments
+                                                .BLOCK_FORTUNE, 0.5714286F, 3)))));
+    }
+    protected LootTable.Builder createLapisOreDrops(Block lapisOre) {
+        return createSilkTouchDispatchTable(lapisOre, this.applyExplosionDecay(lapisOre, LootItem.lootTableItem(Items.LAPIS_LAZULI)
+                .apply(SetItemCountFunction.setCount(UniformGenerator.between(4.0F, 9.0F)))
+                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))));
+    }
+
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
